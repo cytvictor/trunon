622ba22860ed1d42fcf812c275ac419c6be47173
Martian <cytvictor@users.noreply.github.com>
2018-03-19 00:30:16 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
-apt-get update
-apt-get install python python-pip php5 php5-cli php5-curl php5-sqlite \
 libapache2-mod-php5 apache2 -y
-apt-get install composer
+yum install composer
 pip install supervisor
-service apache2 start
